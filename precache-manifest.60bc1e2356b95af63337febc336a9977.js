df44fd755628ceb2794ed8aeb1a2c9858eba15bc
Rostyk Hlynka <hlynka1608@gmail.com>
2019-04-22 02:52:59 +0300

    Updates

---
+++ precache-manifest.60bc1e2356b95af63337febc336a9977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "858c7e0b9cb5126ce1cf",
+    "url": "/istudent/static/js/runtime~main.70018853.js"
+  },
+  {
+    "revision": "4721b620bda80a379084",
+    "url": "/istudent/static/js/main.e5149a71.chunk.js"
+  },
+  {
+    "revision": "f2d126e0750b0d41b0cd",
+    "url": "/istudent/static/js/2.b71a8e9c.chunk.js"
+  },
+  {
+    "revision": "4721b620bda80a379084",
+    "url": "/istudent/static/css/main.ee73b385.chunk.css"
+  },
+  {
+    "revision": "f2d126e0750b0d41b0cd",
+    "url": "/istudent/static/css/2.c16b0e8a.chunk.css"
+  },
+  {
+    "revision": "fdd14ee9d52f8dd8b9ab2f081cb003c8",
+    "url": "/istudent/index.html"
+  }
+];
